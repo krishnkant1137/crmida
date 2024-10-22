300c8ed1438ad11da083e097f8409db6adaf4e96
kmalviya986@gmail.com <your-email@example.com>
2024-10-22 16:04:05 +0530

    demo commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Explicitly set the output directory
+    outDir: 'dist',
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://3.145.137.229:5000', // Replace with your backend server
+        target: 'http://3.145.137.229:5000', 
         changeOrigin: true,
         secure: false,
       },
